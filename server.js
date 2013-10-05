7240fc7ed2a89c0d445f02f53f0a5389b5a742fc
emoore24 <evan.moore24@gmail.com>
2013-10-05 18:53:54 -0400

    fix

--- server.js
+++ server.js
@@ -1,5 +1,40 @@
+// var express = require('express'),
+//     h5bp = require('h5bp');
+
+// var app = express();
+
+// app.configure(function(){
+//   app.set('port', process.env.PORT || 3000);
+//   app.set('views', __dirname + '/views');
+//   app.set('view engine', 'ejs');
+//   // app.use(express.favicon());
+//   // app.use(express.logger('dev'));
+//   // app.use(express.bodyParser());
+//   // app.use(express.methodOverride());
+//   // app.use(app.router);
+//   // app.use(require('stylus').middleware(__dirname + '/public'));
+//   // app.use(express.static(path.join(__dirname, 'public')));
+// });
+
+// app.engine('.html', require('ejs').renderFile);
+
+// app.use(h5bp({ root: __dirname + '/public' }));
+
+// // in order to serve files, you should add the two following middlewares
+// app.use(express.compress());
+// app.use(express.static(__dirname + '/public'));
+
+// app.get('/', function(req, res){
+//   res.render('index.html');
+// });
+
+// app.listen(3000);
+// console.log("server listening");
+
 var express = require('express'),
-    h5bp = require('h5bp');
+    h5bp = require('h5bp'),
+    http = require('http'),
+  path = require('path');
 
 var app = express();
 
@@ -7,26 +42,26 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
-  // app.use(express.favicon());
-  // app.use(express.logger('dev'));
-  // app.use(express.bodyParser());
-  // app.use(express.methodOverride());
-  // app.use(app.router);
-  // app.use(require('stylus').middleware(__dirname + '/public'));
-  // app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'static')));
 });
 
 app.engine('.html', require('ejs').renderFile);
 
-app.use(h5bp({ root: __dirname + '/public' }));
+// app.use(h5bp({ root: __dirname + '/public' }));
 
 // in order to serve files, you should add the two following middlewares
-app.use(express.compress());
-app.use(express.static(__dirname + '/public'));
+// app.use(express.compress());
+// app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.render('index.html');
 });
 
-app.listen(3000);
-console.log("server listening");
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
