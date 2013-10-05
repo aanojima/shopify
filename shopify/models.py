25d9fb9c61764fe0d001907994f3643fc88db037
Aaron Nojima <aanojima@mit.edu>
2013-10-05 15:55:36 -0400

    Agent and Client Models

--- shopify/models.py
+++ shopify/models.py
@@ -10,14 +10,20 @@ from django.forms import ModelForm
 class User(models.Model):
     first_name = models.CharField(max_length = 30)
     last_name = models.CharField(max_length = 40)
-    address = models.ForeignKey(Address)
+    address = models.OneToOneField(Address)
+    profile_picture = models.ImageField(upload_to_LOCATION)
 
 class Address(models.Model):
-	mobile_number = PhoneNumberField(blank=True)
-	home_number = PhoneNumberField(blank=True)
-	fax_number = PhoneNumberField(blank=True)
-	address = models.CharField(max_length=30)
-	city = models.CharField(max_length=30)
-	state = USStateField()
-	zip_code = USPostalCodeField()
+    mobile_number = PhoneNumberField(blank=True)
+    home_number = PhoneNumberField(blank=True)
+    address = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    state = USStateField()
+    zip_code = USPostalCodeField()
+
+class Agent(models.Model):
+
+class Client(models.Model):
+
+
 
