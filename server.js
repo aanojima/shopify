d4b18382237468e85471eb8da268ef78f83240c6
Eric Schmidt <eschmidt@mit.edu>
2013-10-06 05:57:42 -0400

    First deploy

--- server.js
+++ server.js
@@ -50,6 +50,10 @@ app.get('/test', function(req, res) {
   res.render('test');
 });
 
+app.post('/request/new', function(req, res){
+	// handle req.body
+})
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
